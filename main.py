5ba213f59d186d83308fd2471a9deca117c0f7ff
CreatorSakshamGupta <101493002+CreatorSakshamGupta@users.noreply.github.com>
2023-06-24 05:35:13 -0700

    main.py

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-import argparse
-import config
-from weather.api import fetch_weather_data
-from weather.parser import parse_weather_data
-from weather.models import WeatherData
-from weather.utils import display_weather_info
+import requests
+import json
+import matplotlib.pyplot as plt
+
+def get_weather(city):
+    api_key = "41538b3bc8f99dd331c9251ec38b04f4"  # Replace with your OpenWeatherMap API key
+    base_url = "http://api.openweathermap.org/data/2.5/onecall"
+
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"  # Change units to "imperial" for Fahrenheit
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    return data
+
+
+def parse_weather_data(data):
+    if data["cod"] != 200:
+        raise Exception(f"Error: {data['message']}")
+
+    main_info = data["current"]["weather"][0]["main"]
+    description = data["current"]["weather"][0]["description"]
+    temperature = data["current"]["temp"]
+    humidity = data["current"]["humidity"]
+
+    hourly_temperatures = [hour_data["temp"] for hour_data in data["hourly"][:24]]
+
+    return main_info, description, temperature, humidity, hourly_temperatures
+
 
 def main():
-    # Command-line argument parsing
-    parser = argparse.ArgumentParser(description="Weather Forecasting Tool")
-    parser.add_argument("city", help="City name for weather forecast")
-    args = parser.parse_args()
+    city = input("Enter a city name: ")
+    try:
+        print("Fetching weather data...")
+        weather_data = get_weather(city)
+        print("Weather data fetched successfully!")
 
-    # Fetch weather data
-    response = fetch_weather_data(args.city, config.API_KEY)
-    if response.status_code == 200:
-        weather_data = parse_weather_data(response.json())
-        if weather_data:
-            # Create WeatherData object
-            weather_obj = WeatherData(args.city, weather_data)
-            # Display weather information
-            display_weather_info(weather_obj)
-        else:
-            print("Failed to parse weather data.")
-    else:
-        print(f"Failed to fetch weather data. Error: {response.status_code}")
+        main_info, description, temperature, humidity, hourly_temperatures = parse_weather_data(weather_data)
+
+        print(f"Weather in {city}:")
+        print(f"Condition: {main_info} ({description})")
+        print(f"Temperature: {temperature}°C")
+        print(f"Humidity: {humidity}%")
+
+        # Plotting the temperature chart
+        plt.plot(hourly_temperatures, color='orange')
+        plt.title(f"Temperature Forecast for {city}")
+        plt.xlabel("Time (hours)")
+        plt.ylabel("Temperature (°C)")
+        plt.grid(True)
+        plt.show()
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 if __name__ == "__main__":
