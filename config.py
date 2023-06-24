9dfa7a36f019a18ad27aa99d0cda02986984e787
CreatorSakshamGupta <101493002+CreatorSakshamGupta@users.noreply.github.com>
2023-06-24 05:39:04 -0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-API_KEY = config.get("API", "API_KEY")
-BASE_URL = config.get("API", "BASE_URL")
-ENDPOINT = config.get("API", "ENDPOINT")
